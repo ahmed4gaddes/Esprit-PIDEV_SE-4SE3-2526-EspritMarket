c3bed6deef9dab6771f375540a8ecd95ab7cf261
gaddesahmed <gaddesahmed74@gmail.com>
2026-02-17 02:14:35 +0100

    Revert "Merge pull request #1 from hdhilitasnim/feature/Administration"
    This reverts commit 8793e853e40c0bc3ab56af40c098bcf838018103, reversing
    changes made to a11d655954d384dbc75024cc27dbf899b81dc424.
    
    A
    git revert -m 1 8793e85#

--- backend/src/main/java/tn/esprit/esprit_market/modulesAdministration/entities/FinancialReport.java
+++
@@ -1,4 +0,0 @@
-package tn.esprit.esprit_market.modulesAdministration.entities;
-
-public class FinancialReport {
-}
--- backend/src/main/java/tn/esprit/esprit_market/modulesAdministration/enums/RuleCategory.java
+++
@@ -1,4 +0,0 @@
-package tn.esprit.esprit_market.modulesAdministration.enums;
-
-public enum RuleCategory {
-}
