49f63d36dbc3f9ddf1a1d826a4ad107eb8dfa6d2
Majed <majed.benmansour@esprit.tn>
2026-02-22 15:34:51 +0100

    Test

--- backend/src/main/java/tn/esprit/esprit_market/Module/service/enums/RegistrationStatus.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.esprit_market.Module.service.enums;
-
-public enum RegistrationStatus {
-    REGISTERED, CONFIRMED, CANCELLED, COMPLETED
-}
--- backend/src/main/java/tn/esprit/esprit_market/Module/service/enums/ServiceType.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.esprit_market.Module.service.enums;
-
-public enum ServiceType {
-    WORKSHOP, CERTIFICATE, INTERNSHIP
-}
