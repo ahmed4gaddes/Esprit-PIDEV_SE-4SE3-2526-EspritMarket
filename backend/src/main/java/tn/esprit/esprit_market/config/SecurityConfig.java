a97ad7511eea6949a78dc9df7bedf90fb6ad01f4
gaddesahmed <gaddesahmed74@gmail.com>
2026-03-28 20:32:20 +0100

    cors fixed stt

--- backend/src/main/java/tn/esprit/esprit_market/config/SecurityConfig.java
+++ backend/src/main/java/tn/esprit/esprit_market/config/SecurityConfig.java
@@ -97,7 +97,11 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOriginPatterns(List.of("*"));
+    configuration.setAllowedOrigins(List.of(
+      "http://localhost:4200",
+      "http://127.0.0.1:4200",
+      "http://192.168.1.230:30080"
+    ));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
