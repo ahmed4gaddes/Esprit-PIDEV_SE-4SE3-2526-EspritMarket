3ae77dad6eaa098a30a776160e457068c6d21809
gaddesahmed <gaddesahmed74@gmail.com>
2026-03-30 23:38:20 +0100

    test bac2k

--- backend/src/test/java/tn/esprit/esprit_market/config/JwtAuthenticationFilterTest.java
+++ backend/src/test/java/tn/esprit/esprit_market/config/JwtAuthenticationFilterTest.java
@@ -3,6 +3,7 @@ package tn.esprit.esprit_market.config;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -35,6 +36,11 @@ class JwtAuthenticationFilterTest {
 
     @Mock
     private FilterChain filterChain;
+
+    @BeforeEach
+    void setup() {
+        SecurityContextHolder.clearContext();
+    }
 
     @AfterEach
     void clearSecurityContext() {
